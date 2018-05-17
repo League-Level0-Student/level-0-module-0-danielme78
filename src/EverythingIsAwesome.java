cca1ba611280e36f4db036aa025d006e44ec72de
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-17 12:43:05 -0700

    *checkpoint 0

--- src/EverythingIsAwesome.java
+++ src/EverythingIsAwesome.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 public class EverythingIsAwesome {
 	public static void main (string []args) {
 		String answer = JOptionPane.showInputDialog("What's your favorite food?");
-		
+		// change
 	}
 	
 
